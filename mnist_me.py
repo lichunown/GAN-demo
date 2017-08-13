5946b6b15a973671668d5c60209852456bbff089
LCY <lichunyang_1@outlook.com>
2017-08-13 14:44:01 +0800

    first commit

---
+++ mnist_me.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import time
+from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop
+
+import matplotlib.pyplot as plt
+
+class GAN(object):
+    def __init__(self):
+        self.generator = self._generator_layer()
+        self.discriminator = self._discriminator_layer()
+        self.DM = self._discriminator_model(self.discriminator)
+        self.AM = self._AM_model(self.generator,self.discriminator)
+        self.x_data = None
+        self.batch_size = 32
+        
+    def _generator_layer(self):
+        model = Sequential()
+        model.add(BatchNormalization(input_shape=(100,)))
+        model.add(Dense(7*7*64*4,activation='relu'))
+        model.add(Reshape((7,7,64*4)))
+        model.add(Dropout(0.4))   
+        model.add(UpSampling2D())
+        model.add(Conv2DTranspose(int(64*2), 5, padding='same',activation='relu'))
+        model.add(UpSampling2D())
+        model.add(Conv2DTranspose(int(64), 5, padding='same',activation='relu'))            
+        model.add(Conv2DTranspose(int(32), 5, padding='same',activation='relu'))  
+        model.add(Conv2DTranspose(1, 5, padding='same',activation='sigmoid'))    
+        return model
+    def _discriminator_layer(self):
+        model = Sequential()
+        model.add(Conv2D(64, 5, strides=2, input_shape=(28,28,1),padding='same',activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Conv2D(128, 5, strides=2 ,padding='same',activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Conv2D(64*4, 5, strides=2 ,padding='same',activation='relu'))
+        model.add(Dropout(0.3))        
+        model.add(Conv2D(64*8, 5, strides=1 ,padding='same',activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Flatten())
+        model.add(Dense(1,activation='sigmoid'))
+        return model
+    def _AM_model(self,generator,discriminator):
+        optimizer = RMSprop(lr=0.001, decay=1e-6)
+        AM = Sequential()
+        AM.add(generator)
+        AM.add(discriminator)
+        AM.compile(loss='binary_crossentropy', optimizer=optimizer,metrics=['accuracy'])
+        return AM
+        
+    def _discriminator_model(self,model):
+        DM = Sequential()
+        optimizer = RMSprop(lr=0.002, decay=1e-6)
+        DM.add(model)
+        DM.compile(loss='binary_crossentropy', optimizer=optimizer,metrics=['accuracy'])
+        return DM
+    def train(self,data,train_steps=2000):
+        for train_step in range(train_steps):
+            noise = np.random.uniform(-1.0, 1.0, size=[self.batch_size, 100])
+            x_batch = data[np.random.randint(0,data.shape[0],[self.batch_size]),:,:,:]
+            x_fake = self.generator.predict(noise)
+            x = np.concatenate((x_batch, x_fake))
+            y = np.ones([2*self.batch_size, 1])
+            y[self.batch_size:, :] = 0
+            d_loss = self.DM.train_on_batch(x, y)
+            
+            y = np.ones([self.batch_size, 1])
+            #noise = np.random.uniform(-1.0, 1.0, size=[self.batch_size, 100])
+            a_loss = self.AM.train_on_batch(noise, y)
+            if (train_step+1) % 10 == 0:
+                self.plotFake(4)
+                print('{}:  D:[loss:{}  acc:{}]   A:[loss:{}  acc:{}]'.format(train_step+1,d_loss[0],d_loss[1],a_loss[0],a_loss[1]))
+
+    def plotFake(self,num=16):
+        noise = np.random.uniform(-1.0, 1.0, size=[num, 100])
+        fakeimg = self.generator.predict(noise)
+        H = int(np.floor(np.sqrt(num)))
+        L = int(np.ceil(np.sqrt(num)))
+        for i in range(H*L):
+            plt.subplot(H,L,i+1)
+            plt.imshow(fakeimg[i,:,:,:].reshape([28,28]), cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        plt.show()
+    
+    def save_weights(self,name):
+        self.generator.save_weights(name+'.generator')
+        self.discriminator.save_weights(name+'.discriminator')
+        
+    def load_weights(self,name):
+        self.generator.load_weights(name+'.generator')
+        self.discriminator.load_weights(name+'.discriminator')
+
+def plotImgs(imgs):
+    num = len(imgs)
+    H = int(np.floor(np.sqrt(num)))
+    L = int(np.ceil(np.sqrt(num)))
+    for i in range(H*L):
+        plt.subplot(H,L,i+1)
+        plt.imshow(imgs[i], cmap='gray')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+        
+x_train = input_data.read_data_sets("mnist",one_hot=True).train.images
+x_train = x_train.reshape([len(x_train),28,28,1])
+
+gan = GAN()
+
+try:
+    gan.load_weights('test')
+    print('[Message] LOAD weight name `{}`'.format('test'))
+except Exception:
+    print('[Message] random init weight.')
+    pass
+gan.train(x_train)
+
+
+
+
+
+
+
+
